94a20d7eb3b7768f91b2040b27e7fa92849a9285
kakintse <kinkatse@mail.adelphi.edu>
2023-11-25 20:17:04 -0500

    Test all CRUD operations advanced

---
+++ prisma/migrations/20231125225526_create_diff_relations/migration.sql
@@ -0,0 +1,81 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BASIC', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "age" INTEGER,
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "phone" INTEGER,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'BASIC',
+ADD COLUMN     "username" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserPreference" (
+    "id" TEXT NOT NULL,
+    "emailUpdates" BOOLEAN NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "UserPreference_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PostToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPreference_userId_key" ON "UserPreference"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PostToUser_AB_unique" ON "_PostToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PostToUser_B_index" ON "_PostToUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_phone_idx" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserPreference" ADD CONSTRAINT "UserPreference_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToUser" ADD CONSTRAINT "_PostToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToUser" ADD CONSTRAINT "_PostToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- script.ts
+++ script.ts
@@ -1,18 +1,242 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ["query"] })
 
 async function main() {
     // const user = await prisma.user.create({
     //     data: {
-    //         name: "Kin"
+    //         name: "Spencer",
+    //         username: "siascone",
+    //         email: "siascone@aa.io",
+    //         age: 35,
+    //         userPreference: {
+    //             create: {
+    //                 emailUpdates: true
+    //             }
+    //         }
+    //     },
+    //     select: {
+    //         id: true,
+    //         name: true,
+    //         username: true,
+    //         age: true,
+    //         email: true,
+    //         phone: true,
+    //         userPreference: true
     //     }
     // })
 
-    // const user = await prisma.user.findFirst({ where: { name: "Kin" }})
+    // const users = await prisma.user.createMany({
+    //     data: [
+    //         {
+    //             name: "Kin",
+    //             username: "Kinka",
+    //             email: "kinka@im.io",
+    //             age: 27
+    //         },
+    //         {
+    //             name: "Kyle",
+    //             username: "Kginz",
+    //             email: "kginz@aa.io",
+    //             age: 31
+    //         },
+    //         {
+    //             name: "Ayce",
+    //             username: "Alacap",
+    //             email: "alacap@aa.io",
+    //             age: 28
+    //         }
+    //     ]
+    // })
 
-    // const user = await prisma.user.deleteMany()
+    // const foundUser = await prisma.user.findFirst({
+    //     where: { name: "Spencer" },
+    //     select: { id: true, name: true, username: true, age: true, email: true, phone: true },
+    //     // include: { userPreference: true }
+    // })
+
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         username_email: {
+    //             username: 'Kinka',
+    //             email: 'kinka@im.io'
+    //         }
+    //     }
+    // })
+
+    // const users = await prisma.user.createMany({
+    //     data: [
+    //         {
+    //             name: "Kyle",
+    //             username: "kyleA",
+    //             email: "kyleA@example.io",
+    //             age: 15
+    //         },
+    //         {
+    //             name: "Kyle",
+    //             username: "kyleB",
+    //             email: "kginz@example.io",
+    //             age: 15
+    //         },
+    //         {
+    //             name: "Kyle",
+    //             username: "kginz",
+    //             email: "kyleC@example.io",
+    //             age: 15
+    //         },
+    //     ]
+    // })
+
+    // const user = await prisma.user.findMany({
+    //     where: {
+    //         name: "Kyle"
+    //     },
+    //     distinct: ["username", "email"]
+    //     // take: 2,
+    //     // skip: 1
+    // })
+
+    // const user = await prisma.user.findMany({
+    //     where: {
+    //         name: {
+    //             // equals: "Kin"
+    //             in: ["Spencer", "Kin"],
+    //             // notIn: ["Spencer", "Kin"],
+    //             not: "Kin"
+    //         },
+    //         age: { gte: 35 }
+    //     }
+    // })
+
+    // const user = await prisma.user.findMany({
+    //     where: {
+    //         email: {
+    //             // contains: "@example.io"
+    //             endsWith: "@example.io"
+    //         }
+    //     }
+    // })
+
+    // const user = await prisma.user.findMany({
+    //     where: {
+    //         AND: [{
+    //             email: { startsWith: "k" },
+    //             name: { notIn: [ "Kyle" ] }
+    //         }]
+    //         // Can use OR and NOT here as well
+    //         // OR with array and NOT without array
+    //     }
+    // })
+
+    // const user = await prisma.user.findMany({
+    //     where: {
+    //         userPreference: {
+    //             emailUpdates: true
+    //         }
+    //     }
+    // })
+
+    // const user = await prisma.user.findMany({
+    //     where: {
+    //         posts: {
+    //             some: {
+    //                 title: {
+    //                     contains: "Title"
+    //                 }
+    //             }
+    //         }
+    //     }
+    // })
+
+    // const users = await prisma.user.findMany()
+
+    // const postA = await prisma.post.create({
+    //     data: {
+    //         title: "Kin Test",
+    //         body: "Hey this is a test post",
+    //         authorId: user?.id
+    //     }
+    // })
+
+    // const postB = await prisma.post.create({
+    //     data: {
+    //         title: "Kin Title",
+    //         body: "Hey this is another test post",
+    //         authorId: user?.id
+    //     }
+    // })
+
+    const posts = await prisma.post.findMany({
+        where: {
+            author: {
+                is: {
+                    age: 27
+                }
+            }
+        }
+    })
+
+    // const updatedUser = await prisma.user.update({
+    //     where: { email: "kinka@im.io" },
+    //     data: { age: { decrement: 2 } }
+    // })
+
+    // const updatedUser = await prisma.user.update({
+    //     where: { email: "alacap@aa.io" },
+    //     data: { userPreference: {
+    //         create: {
+    //             emailUpdates: true
+    //         }
+    //     } },
+    //     select: {
+    //         id: true,
+    //         name: true,
+    //         username: true,
+    //         age: true,
+    //         email: true,
+    //         phone: true,
+    //         userPreference: true
+    //     }
+    // })
+
+
+    // This wont work because userPreference needs an author
+    // We can adjust our schema so that its optional and then test
+    // this but I dont see myself using this too often
+
+    // const preference = await prisma.userPreference.create({
+    //     data: {
+    //         emailUpdates: true
+    //     }
+    // })
+
+    // const updatedUser = await prisma.user.update({
+    //     where: { email: "alacap@aa.io" },
+    //     data: { userPreference: {
+    //         connect: {
+    //             id: preference?.id
+    //         }
+    //     } }
+    // })
+
+    // const updatedUser = await prisma.user.update({
+    //     where: { email: "alacap@aa.io" },
+    //     data: { userPreference: {
+    //         disconnect: true
+    //     } }
+    // })
+
+    // const posts = await prisma.post.findMany()
+
+    // const deletedPreferences = await prisma.userPreference.deleteMany()
+    // const deletedUsers = await prisma.user.deleteMany()
 
     // console.log(user)
+    // console.log(foundUser)
+    // console.log(users)
+    // console.log(posts)
+    // console.log(updatedUser)
+    // console.log(deletedUsers)
+    // console.log(deletedPreferences)
 }
 
 main()
